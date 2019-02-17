60b8d04eb454dc6c5d0f51241ace81dcad4211f0
E <elee444@gmail.com>
2019-02-17 11:22:08 -0600

    testing test.py push

--- test.py
+++ test.py
@@ -4,3 +4,5 @@ rooms=[{'id': '2031139f-7546-4ecb-8932-374a22a17b3b',
         'name': 'Test_room_2', 'nb_players': 1, 'capacity': 10}]
 y=list(filter(lambda room: room['name'] == 'Test_room_2', rooms))
 print(y)
+print(y)
+#End
