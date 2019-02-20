0a7bbff2b8279f7c52fb19f77840009d6df367b5
Enoch Lee <elee444@gmail.com>
2019-02-20 11:21:47 -0600

    fixed discrepancy. Don't know why

--- client.py
+++ client.py
@@ -1,10 +1,7 @@
 import json
 import threading
 import socket
-<<<<<<< HEAD
-=======
 import time
->>>>>>> f2718d7865ebe578f935fec0fe47b4a3a475d2cd
 
 
 class Client:
@@ -95,11 +92,8 @@ class Client:
         """
         Get the list of (all) remote rooms
         E.g. returns two rooms in a list of dict -
-<<<<<<< HEAD
-=======
         {"id": id_room,"name": room.name,"nb_players": len(room.players),
         "capacity": room.capacity})
->>>>>>> f2718d7865ebe578f935fec0fe47b4a3a475d2cd
         [{'id': '2031139f-7546-4ecb-8932-374a22a17b3b',
         'name': 'Test_room_1', 'nb_players': 3, 'capacity': 10},
         {'id': '2e89bda2-e0f8-486c-aa32-76c1919c40e1',
@@ -157,11 +151,9 @@ class Client:
         Parse response from server
         """
         try:
-<<<<<<< HEAD
-            data = json.loads(data)
-=======
+
+            #data = json.loads(data)
             data = json.loads(data.decode('utf-8')) #Lee add utf-8
->>>>>>> f2718d7865ebe578f935fec0fe47b4a3a475d2cd
             if data['success'] == "True":
                 return data['message']
             else:
@@ -214,7 +206,7 @@ if __name__ == "__main__":
     """
     print("Start testing ....")
     #  Register on server
-<<<<<<< HEAD
+
     client1 = Client("127.0.0.1", 1234, 1234, 1235)
     print("Client 1 : %s" % client1.identifier)
     client2 = Client("127.0.0.1", 1234, 1234, 1236)
@@ -241,60 +233,8 @@ if __name__ == "__main__":
 
         # Get first room for tests
         selected_room1 = rooms[0]['id']  # first room
-
-
         print("Rooms are ", selected_room1)
-=======
-    client0 = Client("127.0.0.1", 1234, 1234, 1230)
-    client1 = Client("127.0.0.1", 1234, 1234, 1235)
-    #print("Client 1 : %s" % client1.identifier)
-    client2 = Client("127.0.0.1", 1234, 1234, 1236)
-    #print("Client 2 : %s" % client2.identifier)
-    client3 = Client("127.0.0.1", 1234, 1234, 1237)
-    #print("Client 3 : %s" % client3.identifier)
-
-    client4 = Client("127.0.01", 1234, 1234, 1238)
-    #print("Client 4 : %s" % client4.identifier)
-    client5 = Client("127.0.01", 1234, 1234, 1239)
-    #print("Client 5 : %s" % client5.identifier)
-
-    #  Create a room on server
-    client0.create_room("Test_room_0")
-    client1.create_room("Test_room_1")
-    #print("Client1 create room  %s" % client1.room_id)
-
-    client4.create_room("Test_room_2")
-    #print("Client4 create room  %s" % client4.room_id)
-    time.sleep(0.01)
-    #  Get rooms list
-    room1s = client1.get_rooms()
-    room2s = client4.get_rooms()
-
-    selected_room1 = None
-    selected_room2= None
-    if room1s is not None and len(rooms1) != 0:
-        print("# rooms = ",len(room1s))
-
-        for room in room1s:
-            print("Room %s (%d/%d)" % (room["name"],
-                                       int(room["nb_players"]),
-                                       int(room["capacity"])))
-    if room1s is not None and len(rooms1) != 0:
-        print("# rooms = ",len(room1s))
-
-        for room in room2s:
-            print("Room %s (%d/%d)" % (room["name"],
-                                       int(room["nb_players"]),
-                                       int(room["capacity"])))
-        # Get first room for tests
-        time.sleep(2)
-        selected_room1 = room1s[0]['id']  # first room
-
-
-        selected_room2 = room2s[0]['id']  # second room
-
         #print("Rooms are ", selected_room1, selected_room2)
->>>>>>> f2718d7865ebe578f935fec0fe47b4a3a475d2cd
     else:
         print("No rooms")
 
@@ -305,7 +245,6 @@ if __name__ == "__main__":
     except Exception as e:
         print("Error : %s" % str(e))
 
-<<<<<<< HEAD
     print("Client 2 join %s" % client2.room_id)
     print("Client 3 join %s" % client3.room_id)
 
@@ -316,47 +255,25 @@ if __name__ == "__main__":
     autorooms = autoclient.get_rooms()
     print("Client auto rooms:", autorooms)
     #autoclient.autojoin()
-=======
+
     #print("Client 2 join %s" % client2.room_id)
     #print("Client 3 join %s" % client3.room_id)
 
-    # joint client 4 room
-    try:
-        client5.join_room(selected_room2)
-    except Exception as e:
-        print("Error : %s" % str(e))
-
-    #print("Client 5 join %s" % client5.room_id)
-
-    client5rooms = client5.get_rooms()
-    #print("Client5 get rooms:", client5rooms)
 
     #autoclient = Client("127.0.0.1", 1234, 1234, 1350)
     #print("Client auto : %s" % autoclient.identifier)
     #autorooms = autoclient.get_rooms()
     #print("Client auto rooms:", autorooms)
     # autoclient.autojoin()
->>>>>>> f2718d7865ebe578f935fec0fe47b4a3a475d2cd
+
     #print("Slave Client  join %s " % autoclient.room_id)
 
     #  Main game loop
     while True:
         #  Send message to room (any serializable data)
-<<<<<<< HEAD
-        client1.send({"name": "John D.",
-                      "message": "I'm just John Doe..."})
-        client2.send({"name": "Linus T.",
-                      "message": "My name is Linus, and I am your God."})
-        client3.send({"name": "Richard S.",
-                      "message": "I love emacs"})
 
 
-        # get server data (only client 3)
-        message = client1.get_messages()
-        if len(message) != 0:
-            for message in message:
-                message = json.loads(message.decode())
-=======
+
         client1.send({"name": "Master2.718",
                       "message": "I'm just John Doe...",
                       "title":client1.identifier})
@@ -366,18 +283,13 @@ if __name__ == "__main__":
         client3.send({"name": "Richard S.",
                       "message": "I love emacs",
                       "title":client1.identifier})
-        client4.send({"name": "John Lennon",
-                      "message": "My name is John, and I am a robot.",
-                      "title":client1.identifier})
-        client5.send({"name": "Ringo Starr",
-                      "message": "I love music",
-                      "title":client1.identifier})
+
 
         # get server data (only client 3)
         messages = client1.get_messages()
         if len(messages) != 0:
             for message in messages:
-                message = json.loads(message)
->>>>>>> f2718d7865ebe578f935fec0fe47b4a3a475d2cd
+                message = json.loads(message.decode())
+
                 sender, value = message.popitem()
                 print("%s say %s" % (value["name"], value["message"]))
