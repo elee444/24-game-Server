3f09f3f5b55b2a1fbc762eb2f221851ef5baffbf
E <elee444@gmail.com>
2019-02-22 11:45:56 -0600

    adding game...

--- gslave1.py
+++ gslave1.py
@@ -4,36 +4,23 @@ import socket
 from client import *
 import argparse
 
+msgpack={"ready1":"R1", #"Rn"=ready to play with n players
+         "ready2":"R2",
+         "ready3":"R3",
+         "ready3+":"R3+", #"Rn+"=ready to play with n or more players
+         "submit":"Sn", #"Sn"=Submit a number for verification
+         "question":"Qn,n,n,n" #"Qn,n,n,n"=The four numbers are n,n,n,n
+         }
+
+#get a msgpack from the master. Decode it.
+#def decodempack(mp):
 
 
 
 if __name__ == "__main__":
-    """
-    Start a game server
 
-    parser = argparse.ArgumentParser(description='Simple game client')
-    parser.add_argument('--tcpport',
-                        dest='tcp_port',
-                        help='Listening tcp port',
-                        default="1234")
-    parser.add_argument('--udpport',
-                        dest='udp_port',
-                        help='Listening udp port',
-                        default="1234")
-    parser.add_argument('--capacity',
-                        dest='room_capacity',
-                        help='Max players per room',
-                        default="2")
-
-    args = parser.parse_args()
-    """
-
-
-    """
-    Example game client 1
-    """
     #  Register on server
-    gclient = Client("127.0.0.1", 1234, 1234, find_free_port())
+    gclient = Client('localhost', 1234, 1234, find_free_port())
 
     print("gClient  : %s" % gclient.identifier)
 
@@ -60,6 +47,9 @@ if __name__ == "__main__":
 
     print("gClient  join %s" % gclient.room_id)
 
+    #signal the master I am ready1
+    gclient.send({"name": "Client 1",
+                  "message":msgpack["ready1"]})
 
     #  Main game loop
     while True:
